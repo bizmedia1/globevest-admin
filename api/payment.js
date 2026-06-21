1390c574716164cc10443bc71eb88e0384f750aa
bizmedia1 <victorisrael539@gmail.com>
2026-06-21 11:43:42 +0100

    Update payment.js

--- api/payment.js
+++ api/payment.js
@@ -1,5 +1,20 @@
 export default function handler(req, res) {
 
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "*"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type"
+  );
+
   res.status(200).json({
     accountNumber: "0941410430",
     bankName: "Pagiaa",
