879115f3c662220857fa30db46c13055829fdc80
bizmedia1 <victorisrael539@gmail.com>
2026-06-21 11:13:48 +0100

    Update payment.js

--- api/payment.js
+++ api/payment.js
@@ -2,8 +2,8 @@ export default function handler(req, res) {
 
   res.status(200).json({
     accountNumber: "0941410430",
-    bankName: "Paga",
-    accountName: "ASPFIY",
+    bankName: "Pagiaa",
+    accountName: "ASPFkwjIY",
     telegram: "globevest_support"
   });
 
